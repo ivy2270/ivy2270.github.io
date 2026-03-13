e295ac9c87b3f6a84409ba34fd9562d25dcd21f9
ivy2270 <042703896@yahoo.com.tw>
2026-03-14 03:17:33 +0800

    Create sw.js

---
+++ 2026-gtidec/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'dent-order-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  'https://font.emtech.cc/css/jfOpenHuninn',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+// 安裝並快取資源
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// 攔截請求，優先使用快取
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
